15a651062dbdb624381b2fae1aac34296d4f549d
Pedro Fires <pedrof.fires@gmail.com>
2022-06-26 03:27:59 -0300

    created components and another setups

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./lib/apollo";
+import { Router } from "./router";
+
 function App() {
-   return <div></div>;
+   return (
+      <ApolloProvider client={client}>
+         <Router />
+      </ApolloProvider>
+   );
 }
 
 export default App;
